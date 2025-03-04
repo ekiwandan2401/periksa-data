a0cdbb21ae409f104498cb4f81426a90861ff4c0
Eki Wandana Putra <44340001+ekiwandan2401@users.noreply.github.com>
2025-03-04 14:37:49 +0700

    Create spinner.jsx

---
+++ src/components/magicui/spinner.jsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+export default function Spinner({
+  className,
+  outerSize,
+  childSize,
+}) {
+  return (
+    <div
+      className={cn(
+        "m-2 h-8 w-8 animate-spin items-center justify-center rounded-full bg-gradient-to-bl from-pink-500 to-blue-600 p-0.5",
+        className,
+        outerSize,
+      )}
+    >
+      <div className={cn("h-6 w-6 rounded-full bg-white", childSize)} />
+    </div>
+  );
+}
